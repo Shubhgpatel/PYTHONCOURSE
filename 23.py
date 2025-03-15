fa65c41c8d6eb688d7535c4643cb8a401e880023
Shubh Patel <shubhpatel@Shubhs-MacBook-Air.local>
2025-03-15 15:17:16 +0530

    Added 23.py file

---
+++ 23.py
@@ -0,0 +1,7 @@
+# with keyword in file handling 
+
+with open("file1.txt") as f:
+    print(f.read())
+
+# you don't have to explicitly close the file with the help of "with" keyword.
+# with keyword close your file automatically.
